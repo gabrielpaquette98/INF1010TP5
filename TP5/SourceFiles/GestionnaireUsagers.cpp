8229259e82bb1018a4a70f7f8b131855e5a9c393
Vito <vittorio-passduss@outlook.com>
2018-04-06 09:09:40 -0400

    remplacement des for_each s par des boucles for dans GestionnaireUsagers.cpp

--- TP5/SourceFiles/GestionnaireUsagers.cpp
+++ TP5/SourceFiles/GestionnaireUsagers.cpp
@@ -16,15 +16,12 @@ GestionnaireUsagers::~GestionnaireUsagers() {
 
 double GestionnaireUsagers::obtenirChiffreAffaires() const {
 	double chiffreAffaires = 0.0;
-	// TODO utiliser pourChaqueElement
-	std::for_each(
-		conteneur_.begin(),
-		conteneur_.end(),
-		[&](Usager* const usager) { chiffreAffaires += (*usager).obtenirTotalAPayer(); }
-	);
+	for (auto it = conteneur_.begin(); it != conteneur_.end(); it++) {
+		Usager* usager = *it;
+		chiffreAffaires += usager->obtenirTotalAPayer();
+	}
 };
 
-// TODO verifier
 void GestionnaireUsagers::encherir(Client* client, ProduitAuxEncheres* produit, double montant) const {
 	if (montant <= produit->obtenirPrix()) {
 		return;
@@ -35,35 +32,29 @@ void GestionnaireUsagers::encherir(Client* client, ProduitAuxEncheres* produit, 
 };
 
 void GestionnaireUsagers::reinitialiser() {
-	// TODO utiliser pourChaqueElement
-	std::for_each(
-		conteneur_.begin(),
-		conteneur_.end(),
-		[](Usager* usager) { (*usager).reinitialiser(); }
-	);
+	for (auto it = conteneur_.begin(); it != conteneur_.end(); it++) {
+		Usager* usager = *it;
+		usager->reinitialiser();
+	}
 };
 
 void GestionnaireUsagers::afficherProfils() const {
-	// TODO utiliser pourChaqueElement
-	std::for_each(
-		conteneur_.begin(),
-		conteneur_.end(),
-		[](Usager* usager) { 
-			if (dynamic_cast<Fournisseur*>(usager) != nullptr) {
-				Fournisseur fournisseurTemp = *dynamic_cast<Fournisseur*>(usager);
-				fournisseurTemp.afficher();
-			}
-			else if (dynamic_cast<ClientPremium*>(usager) != nullptr) {
-				ClientPremium clientPremiumTemp = *dynamic_cast<ClientPremium*>(usager);
-				clientPremiumTemp.afficher();
-			}
-			else if (dynamic_cast<Client*>(usager) != nullptr) {
-				Client clientTemp = *dynamic_cast<Client*>(usager);
-				clientTemp.afficher();
-			}
-			else {
-				std::cout << '\t' << '\t' << std::left << "PROFIL INVALIDE" << endl;
-			}
+	for (auto it = conteneur_.begin(); it != conteneur_.end(); it++) {
+		Usager* usager = *it;
+		if (dynamic_cast<Fournisseur*>(usager) != nullptr) {
+			Fournisseur fournisseurTemp = *dynamic_cast<Fournisseur*>(usager);
+			fournisseurTemp.afficher();
 		}
-	);
+		else if (dynamic_cast<ClientPremium*>(usager) != nullptr) {
+			ClientPremium clientPremiumTemp = *dynamic_cast<ClientPremium*>(usager);
+			clientPremiumTemp.afficher();
+		}
+		else if (dynamic_cast<Client*>(usager) != nullptr) {
+			Client clientTemp = *dynamic_cast<Client*>(usager);
+			clientTemp.afficher();
+		}
+		else {
+			std::cout << '\t' << '\t' << std::left << "PROFIL INVALIDE" << endl;
+		}
+	}
 }
